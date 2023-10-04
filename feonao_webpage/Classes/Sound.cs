2ce6bf86429bd6c87768513fe6d39ec60ae9f6db
Maxim Potapov <potapov@t8.ru>
2023-10-04 15:00:41 +0400

    Video page content

--- feonao_webpage/Classes/Sound.cs
+++ feonao_webpage/Classes/Sound.cs
@@ -1,14 +1,7 @@
 ﻿namespace feonao.Classes
 {
-    public class Sound
+    public class Sound : Media
     {
-        private string playClass = "text-primary oi oi-media-play me-2";
-        private string pauseClass = "text-primary oi oi-media-pause me-2";
-
-        public string Name { get; set; }
-        public string Url { get; set; }
-        public string? State { get; set; }
-
         public string Artwork { get; set; }
         public string Platform { get; set; }
         public string Year { get; set; }
@@ -19,28 +12,6 @@
             Url = url;
             Artwork = "";
             SetPlay();
-        }
-
-        public void SetPause()
-        {
-            State = pauseClass;
-        }
-
-        public void SetPlay()
-        {
-            State = playClass;
-        }
-
-        public void ChangeState()
-        {
-            if (State == pauseClass)
-            {
-                State = playClass;
-            }
-            else if (State == playClass)
-            {
-                State = pauseClass;
-            }
         }
     }
 }
---
+++ feonao_webpage/Classes/Video.cs
@@ -0,0 +1,12 @@
+﻿namespace feonao.Classes
+{
+    public class Video : Media
+    {
+        public Video(string name, string url)
+        {
+            Name = name;
+            Url = url;
+            SetPlay();
+        }
+    }
+}
