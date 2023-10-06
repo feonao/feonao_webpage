7562e945fc5716ba0912918e763d0aad2c118e50
Maxim Potapov <potapov@t8.ru>
2023-10-06 15:13:25 +0400

    Player selection design

--- feonao_webpage/Classes/Media.cs
+++ feonao_webpage/Classes/Media.cs
@@ -2,8 +2,8 @@
 {
     public class Media
     {
-        private string playClass = "text-primary oi oi-media-play me-2";
-        private string pauseClass = "text-primary oi oi-media-pause me-2";
+        private string playClass = "oi oi-media-play me-2";
+        private string pauseClass = "oi oi-media-pause me-2";
 
         public string Name { get; set; }
         public string Url { get; set; }
@@ -28,6 +28,30 @@
             else if (State == playClass)
             {
                 State = pauseClass;
+            }
+        }
+
+        public String BackgroundColor(bool isCurrent)
+        {
+            if (isCurrent)
+            {
+                return "bg-secondary";
+            }
+            else
+            {
+                return "bg-white";
+            }
+        }
+
+        public String BackgroundTextColor(bool isCurrent)
+        {
+            if (isCurrent)
+            {
+                return "text-white";
+            }
+            else
+            {
+                return "text-dark";
             }
         }
     }
