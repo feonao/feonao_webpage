9f08196f1d3105274f46503fa3c587fd90ea8977
Maxim Potapov <potapov@t8.ru>
2023-11-01 14:01:40 +0400

    JS scripts file

---
+++ feonao_webpage/wwwroot/scripts/PlayerScripts.js
@@ -0,0 +1,31 @@
+﻿function ChangePlayerState() {
+    var player;
+
+    player = document.getElementById('audioPlayer');
+
+    if (player.paused) {
+        player.play();
+    }
+    else {
+        player.pause();
+    }
+}
+
+function SetAutoPlay(isEnabled) {
+    var player;
+
+    player = document.getElementById('audioPlayer');
+
+    player.autoplay = isEnabled;
+}
+
+function SetSrc(path) {
+    var player;
+
+    player = document.getElementById('audioPlayer');
+
+    if (player !== null) {
+        player.src = path;
+        player.preload = 'metadata';
+    }
+}
